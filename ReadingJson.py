2a49a11fbf76119895403c4f51aafefcf700777a
awais922609 <awaissajid92260@gmail.com>
2024-10-30 21:47:31 +0500

    json

--- ReadingJson.py
+++ ReadingJson.py
@@ -1,11 +1,20 @@
 import json
 
+
+    #Counting failed attempts
+def numberOfFailedAttempts(data):
+    count = 0
+    for entry in data:
+        if entry['status'] == "failed":
+            count += 1
+    print (f"\n Number of failed attempts : {count}")
+
 with open("./security_log.json" , "r") as file:
     data = json.load(file)
-    # print(data)
-
     for entry in data:
         print(f"Timestamp: {entry['timestamp']}  - Username: {entry['username']} - IP Address: {entry['ip_address']} - Status: {entry['status']} ")
+    
+    numberOfFailedAttempts(data)
 
     #Printing failed logins
 
@@ -14,5 +23,4 @@ with open("./security_log.json" , "r") as file:
             
             print("\n Printing failed Enteries")
             print(f"Timestamp: {entry['timestamp']}  - Username: {entry['username']} - IP Address: {entry['ip_address']} - Status: {entry['status']} ")
-
 
